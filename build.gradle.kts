f9c978ff161428fdb8e40b4b9b0b3d3d4f630a9f
Fabian Thomys <git@fthomys.me>
2025-06-12 14:41:47 +0200

    fix(deps): refactor Skiko dependencies to use version variable and update JUnit test dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -15,18 +15,21 @@ repositories {
     mavenCentral()
     maven("https://maven.pkg.jetbrains.space/public/p/compose/dev")
 }
-
+val skikoVersion = "0.9.17"
 dependencies {
-    implementation("org.jetbrains.skiko:skiko:0.9.17")
-    implementation("org.jetbrains.skiko:skiko-awt-runtime-windows-x64:0.9.17")
-    implementation("org.jetbrains.skiko:skiko-awt-runtime-linux-x64:0.9.17")
-    implementation("org.jetbrains.skiko:skiko-awt-runtime-macos-x64:0.9.17")
-    implementation("org.jetbrains.skiko:skiko-awt-runtime-macos-arm64:0.9.17")
+    implementation("org.jetbrains.skiko:skiko:$skikoVersion")
+    implementation("org.jetbrains.skiko:skiko-awt-runtime-windows-x64:$skikoVersion")
+    implementation("org.jetbrains.skiko:skiko-awt-runtime-linux-x64:$skikoVersion")
+    implementation("org.jetbrains.skiko:skiko-awt-runtime-macos-x64:$skikoVersion")
+    implementation("org.jetbrains.skiko:skiko-awt-runtime-macos-arm64:$skikoVersion")
     implementation("com.squareup.okhttp3:okhttp:4.12.0")
     implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
 
     testImplementation(platform("org.junit:junit-bom:5.13.1"))
-    testImplementation("org.junit.jupiter:junit-jupiter")
+    testImplementation("org.junit.jupiter:junit-jupiter-api")
+    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine")
+    testRuntimeOnly("org.junit.platform:junit-platform-launcher:1.13.1")
+
 }
 
 tasks.test {
